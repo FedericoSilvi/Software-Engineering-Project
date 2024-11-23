6f7134a688b863c224e06a137d8ff9ecd158acdc
Federico Silvi <federico.silvi@studentmail.unicas.it>
2024-11-23 15:41:53 +0100

    Commenti sia per documentazione che infra-codice per le classi DAO

--- src/it/unicas/clinic/address/model/dao/mysql/LoginDAOImplementation.java
+++ src/it/unicas/clinic/address/model/dao/mysql/LoginDAOImplementation.java
@@ -10,58 +10,95 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contain the operations regarding the login operations with SQL database
+ */
+
 public class LoginDAOImplementation {
 
     private String username;
     private String password;
 
+    /**
+     * Constructor with passed arguments
+     * @param us
+     * @param pass
+     */
     public LoginDAOImplementation(String us, String pass) {
         this.username = us;
         this.password = pass;
     }
 
+    /**
+     * Getter of username
+     */
     public String getUsername() {
         return username;
     }
-
+    /**
+     * Getter of username
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
-
+    /**
+     * Getter of password
+     */
     public String getPassword() {
         return password;
     }
-
+    /**
+     * Getter of username
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
+
+    /**
+     * Search for the credentials contained in the Class. If found, search for the
+     * corresponding user and return it, else return null
+     * @throws SQLException
+     */
     public User searchUser() throws SQLException {
+        //Open connection
         Connection connection = DAOMySQLSettings.getConnection();
+        //Define command
         String searchUser = "select * from credential where username=? and password=?";
         PreparedStatement command = connection.prepareStatement(searchUser);
         command.setString(1,this.username);
         command.setString(2,this.password);
+        //Execute command
         ResultSet result = command.executeQuery();
         boolean isManager = false;
-        if(result.next()){
+        if(result.next()){  //If command has found something
             String staff_id="";
+            //Save the info about user being manager or not
             isManager = result.getBoolean("owner");
+            //Get the staff id from credential table
             staff_id = result.getString("staff_id");
+            //Command for searching in staff table
             String staffSearch = "select * from staff where id=?";
             PreparedStatement staff = connection.prepareStatement(staffSearch);
             staff.setString(1,staff_id);
+            //Execute command
             ResultSet staff_data = staff.executeQuery();
+            //Define user to store staff member if found
             User user = new User();
             while(staff_data.next()) {
+                //Save staff member infos
                 user.setName(staff_data.getString("name"));
                 user.setSurname(staff_data.getString("surname"));
                 user.setManager(isManager);
             }
+            //Close connection
             DAOMySQLSettings.closeConnection(connection);
             return user;
         }
 
-        else {
+        else {  // If command hasn't found anything
+            //Close connection
             DAOMySQLSettings.closeConnection(connection);
             return null;
         }
