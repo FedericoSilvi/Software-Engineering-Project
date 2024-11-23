a65c85cbe9142e01359a8b40d1445fcfa6b296b9
Federico Silvi <federico.silvi@studentmail.unicas.it>
2024-11-23 12:25:18 +0100

    Creata la classe User per agevolare le operazioni di login e ilk riconoscimento della giusta schermata da far comparire.

--- src/it/unicas/clinic/address/utils/DataUtil.java
+++ src/it/unicas/clinic/address/utils/DataUtil.java
@@ -1,4 +1,41 @@
 package it.unicas.clinic.address.utils;
 
 public class DataUtil {
+    public static class User{
+
+
+        private String name;
+        private String surname;
+        private boolean isManager;
+
+        public String getName() {
+            return name;
+
+        }
+
+        public void setName(String username) {
+            this.name = username;
+        }
+
+        public String getSurname() {
+            return surname;
+        }
+
+        public void setSurname(String surname) {
+            this.surname = surname;
+        }
+
+        public boolean getManager() {
+            return isManager;
+        }
+
+        public void setManager(boolean manager) {
+            isManager = manager;
+        }
+        public User() {
+            this.name = "";
+            this.surname = "";
+            this.isManager = false;
+        }
+    }
 }
