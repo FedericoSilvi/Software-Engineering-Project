1329aaf4b34f0446ac3ce5aff0ca1fc365f8ae48
Marco Caruso <marco.caruso-3@studentmail.unicas.it>
2024-11-26 15:36:38 +0100

    Implementation of Owner Layout: possibility to choose what to do with a SplitMenu

--- src/it/unicas/clinic/address/Main.java
+++ src/it/unicas/clinic/address/Main.java
@@ -1,16 +1,15 @@
 package it.unicas.clinic.address;
 
 import it.unicas.clinic.address.model.Staff;
-import it.unicas.clinic.address.view.ScheduleAddingLayoutController;
-import it.unicas.clinic.address.view.StaffAddingLayoutController;
-import it.unicas.clinic.address.view.StaffManagementLayoutController;
-import it.unicas.clinic.address.view.StaffUpdateLayoutController;
+import it.unicas.clinic.address.view.*;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -24,6 +23,7 @@ public class Main extends Application {
 
     private Stage primaryStage;
     private ObservableList<Staff> staffData = FXCollections.observableArrayList();
+    private BorderPane page;
     public ObservableList<Staff> getStaffData() {
         return staffData;
     }
@@ -120,6 +120,7 @@ public class Main extends Application {
 
     public void start(Stage primaryStage) throws IOException {
         this.primaryStage=primaryStage;
+        /*this.primaryStage=primaryStage;
         //primaryStage.getIcons().add(new Image("file:resources/clinic-icon-3.png"));
         this.primaryStage.setTitle("Clinic");
         FXMLLoader loader = new FXMLLoader();
@@ -133,9 +134,45 @@ public class Main extends Application {
         StaffManagementLayoutController controller = loader.getController();
         controller.setMainApp(this);
 
-        primaryStage.show();
+        primaryStage.show();*/
+
+        loadStaffManagementChoose();
+        loadStaffManagement();
+
+
 
     }
+    public void loadStaffManagementChoose() throws IOException{
+        primaryStage.getIcons().add(new Image("file:resources/clinic-icon-3.png"));
+        this.primaryStage.setTitle("Clinic");
+        FXMLLoader loader = new FXMLLoader();
+        this.primaryStage.setTitle("Clinic");
+        loader.setLocation(Main.class.getResource("view/ChooseOwnerLayout.fxml"));
+        page =  loader.load();
+        Scene scene = new Scene(page);
+        primaryStage.setScene(scene);
+        ChooseOwnerLayoutController controller = loader.getController();
+        controller.setMain(this);
+    }
+    public void loadStaffManagement() throws IOException{
+        FXMLLoader loader2 = new FXMLLoader();
+        loader2.setLocation(Main.class.getResource("view/StaffManagementLayout2.fxml"));
+        page.setCenter(loader2.load());
+        StaffManagementLayoutController controller2 = loader2.getController();
+        controller2.setMainApp(this);
+        primaryStage.show();
+    }
+    public void loadScheduleManagement() throws IOException{
+        FXMLLoader loader2 = new FXMLLoader();
+        loader2.setLocation(Main.class.getResource("view/ScheduleAddingLayout.fxml"));
+        page.setCenter(loader2.load());
+        ScheduleAddingLayoutController controller2 = loader2.getController();
+        controller2.setMainApp(this);
+        primaryStage.show();
+    }
+
+
+
     /**
      * Returns the main stage.
      * @return
